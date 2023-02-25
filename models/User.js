05affcbafc3198ffd627b214f9f59f66e5d90343
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-02-25 17:45:45 -0500

    added userSchema

--- models/User.js
+++ models/User.js
@@ -4,15 +4,45 @@ const { Schema, model, Types } = require('mongoose');
 // add userSchema
 const userSchema = new Schema(
     { // definitions username, email. thoughts, friends
-
+        username: {
+            type: String,
+            unique: true,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "A valid email address is required!"
+            ],
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "Thought",
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: "User",
+            },
+        ],
     },
     { //options
-
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
 
 // add friendCount virtual
 
 //define User model
+const User = model('User', userSchema);
 
 module.exports = User;
