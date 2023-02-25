0d6fcb4ac8f4aa82a41e8be71455739691388534
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-02-25 16:47:52 -0500

    added model psuedocode structure

--- models/Thought.js
+++ models/Thought.js
@@ -1,3 +1,31 @@
+// imports mongoose methods & moment
+const { Schema, model, Types } = require('mongoose');
+const moment = require('moment');
 
+// add thoughtSchema
+const thoughtSchema = new Schema (
+    { //definitions thoughtText, createdAt, username, reactions
+
+
+    },
+    { //options
+
+    }
+)
+
+// add reactionSchema
+const reactionSchema = new Schema (
+    { //definitions reactionId, reactionBody, username, createdAt
+
+
+    },
+    { //options
+
+    }
+)
+
+// add reactionCount virtual
+
+//define model
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,4 +1,18 @@
+// add imports
+const { Schema, model, Types } = require('mongoose');
 
+// add userSchema
+const userSchema = new Schema(
+    { // definitions username, email. thoughts, friends
 
+    },
+    { //options
+
+    }
+)
+
+// add friendCount virtual
+
+//define User model
 
 module.exports = User;
