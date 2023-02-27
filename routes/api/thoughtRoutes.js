769e26acdc5070de11620e058edbb195c67d1af2
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-02-26 21:27:30 -0500

    added thoughtRoutes .route('s')

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,36 @@
 //imports
+const router = require('express').Router();
+const {
+    getAllThought,
+    getOneThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    addReaction,
+    deleteReaction,
+} = require('../../controllers/thoughtController');
 
-// GET api/thoughts  / GET api/thoughts/:id / POST / PUT api/thoughts/:id / DELETE api/thoughts/:id
+// api/thoughts
+// GET / POST 
+router.route('/')
+.get(getAllThought)
+.post(createThought);
 
-// api/thoughts/:thoughtId/reactionId  POST & DELETE
+// api/thoughts/:id 
+// GET / PUT / DELETE
+router.route('/:thoughtId')
+.get(getOneThought)
+.put(updateThought)
+.delete(deleteThought);
+
+// api/thoughts/:thoughtId/reactions/
+// POST
+router.route('/:thoughtId/reactions')
+.post(addReaction);
+
+// api/thoughts/:thoughtId/reactions/:reactionId
+//  DELETE
+router.route('/:thoughtId/reactions/reactionId')
+.post(deleteReaction);
+
+module.exports = router;
