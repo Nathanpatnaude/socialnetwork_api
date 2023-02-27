ba9de15ddd1cf8de8ddef3e825e7ba1c17625484
Nathan Patnaude <nathanpatnaude@gmail.com>
2023-02-26 21:15:48 -0500

    added userRoutes .route('s')

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,5 @@
+//imports
+
+// GET api/thoughts  / GET api/thoughts/:id / POST / PUT api/thoughts/:id / DELETE api/thoughts/:id
+
+// api/thoughts/:thoughtId/reactionId  POST & DELETE
---
+++ routes/api/userRoutes.js
@@ -0,0 +1,33 @@
+//imports
+const router = require('express').Router();
+const {
+    getAllUser,
+    getOneUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController')
+
+// /api/users 
+// GET / POST
+router.route('/')
+.get(getAllUser)
+.post(createUser);
+
+//  /api/users/:id 
+// GET / PUT / DELETE
+router.route('/:userId')
+.get(getOneUser)
+.put(updateUser)
+.delete(deleteUser);
+
+
+// api/users/:userId/friends/:friendId
+// POST / DELETE
+router.route('/:userId/friends/:friendId')
+.post(addFriend)
+.delete(deleteFriend);
+
+module.exports = router;
